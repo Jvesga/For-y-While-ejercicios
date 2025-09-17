a1359da2fc975674a6aec9724fdc5a31b3e6a0c9
Jorge Alejandro Vesga Quintero <102545608+Jvesga@users.noreply.github.com>
2025-09-16 19:48:55 -0500

    Add files via upload

---
+++ While ejercicio 1.py
@@ -0,0 +1,10 @@
+numeros = 0
+suma_numeros = 0
+
+while numeros != 100:
+    numeros = numeros + 1
+    suma_numeros = suma_numeros + numeros
+
+
+print(f"La suma de los primeros 100 numeros es: {suma_numeros}")  
+    
